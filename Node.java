956adc7293f50a1223cbd168a0e2054c0d09c68b
priyanka-06 <33381265+priyanka-06@users.noreply.github.com>
2018-02-18 13:58:53 -0600

    Add files via upload

---
+++ Node.java
@@ -0,0 +1,26 @@
+public class Node
+{    
+    public Node left, right;
+    public String featureName;
+    public int  a ,b, counter;
+    public double entropy;
+    public boolean data;
+    public Boolean classOutput = null;
+    
+    public Node(int a, int b, double entropy, boolean data, String featureName){
+        left = null;
+        right = null;
+        this.data = data;
+        this.a = a;
+        this.b = b;
+        this.entropy = entropy;
+        this. featureName = featureName;
+    }
+    
+    public Node(boolean data, String featureName){
+        left = null;
+        right = null;
+        this.data = data;
+        this. featureName = featureName;
+    }
+}
